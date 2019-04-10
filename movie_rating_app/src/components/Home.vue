ba1f38746cc8aacd15341c8d00c90024a8f1dc63
Ali <zamanmuhammadali@gmail.com>
2019-04-10 16:09:41 +0200

    Redesigning the app

---
+++ movie_rating_app/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <div v-text="message"></div>
+    </v-flex>
+    <v-flex xs12>
+      <v-btn color="primary" v-on:click="reply">Reply</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+      message: 'Hello there, how are you this morning?',
+    };
+  },
+  methods: {
+    reply() {
+      this.message = "I'm doing great. Thank You!";
+    },
+  },
+};
+</script>
